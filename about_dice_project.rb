1c6e6f177e401d3e28722d75dfff2cadeaa553fe
Volodymyr Stelmakh <volodymyr.stelmakh@wonga.com>
2013-09-06 11:32:01 +0100

    Finished inheritance

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  def roll(num)
+    @dice_set = Array.new(num) { 1 + Random.rand(6) }
+  end
+
+  def values
+    @dice_set
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
